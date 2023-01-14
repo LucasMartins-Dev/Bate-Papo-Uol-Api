3f2a9b9ae889828017e9a4049e30ae2d0550e6fb
LUCAS MARTINS MOREIRA SOUZA <CSO5560@corp.csn.com.br>
2023-01-14 17:22:37 -0300

    return

--- src/app.js
+++ src/app.js
@@ -3,9 +3,8 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
 import dayjs from 'dayjs'
-import joi from 'joi'
+import Joi from 'joi'
 
-const Joi = require('joi');
 
 dotenv.config()
 const app= express()
@@ -43,7 +42,7 @@ app.get('/participants',async (req,res)=>{
     app.post('/participants', async (req,res)=>{
      
         try{
-            const nomeparticipante = await participantsSchema.validateAsync(req.body) 
+            const nomeparticipante = await participantsSchema.validate(req.body) 
             const namexiste = await db.collection('participants').findOne(nomeparticipante)
             if(namexiste) return res.status(409).send("Usuario já cadastrado")
             await db.collection('participants').insertOne({ name:nomeparticipante,lastStatus: Date.now()})
@@ -57,7 +56,7 @@ app.get('/participants',async (req,res)=>{
 
         }catch(err){
             console.log(err)
-            if (nomeparticipante.name) return res.sendStatus(422)
+            if (err.isJoi) return res.sendStatus(422)
             res.status(500).send('Deu erro !!')
         }
        
