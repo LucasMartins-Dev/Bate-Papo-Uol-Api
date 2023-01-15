2fdd51c421bdad84dc9ea0eecadeaeb3759bd8f1
Lucas <lucasmartinsms.dev@gmail.com>
2023-01-15 15:23:26 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -28,26 +28,6 @@ const messageschema = Joi.object({
     type: Joi.string().valid('message','private_message').required()
 })
 
-setInterval(async () => {
-    const valor = Date.now() - 10000
-    let remove = []
-    await db.collection("participants").find({ lastStatus: { $lt: valor } })
-        .toArray()
-        .then(dados => {
-            remove = [...dados]
-        })
-    for (const m of remove) {
-        const messageout = await db.collection("messages").insertOne({
-            from: m.name,
-            to: 'Todos',
-            text: 'sai da sala...',
-            type: 'status',
-            time: dayjs().format("HH:mm:ss")
-        })
-    }
-    await db.collection("participants").deleteMany({ lastStatus: { $lt: valor } });
-}, 15000)
-
 app.get('/participants',async (req,res)=>{
     try{
         const participants = await db.collection("participants").find().toArray()
@@ -73,7 +53,7 @@ app.post('/participants', async (req,res)=>{
                 return res.status(422).send(errors);
               }
             const namexiste = await db.collection('participants').findOne({nome})
-            if(!namexiste) return res.status(409).send("Usuario já cadastrado")
+            if(namexiste) return res.status(409).send("Usuario já cadastrado")
             await db.collection('participants').insertOne({ ...nome,lastStatus: Date.now()})
             await db.collection("messages").insertOne({
                 from: nome.name,
@@ -89,7 +69,7 @@ app.post('/participants', async (req,res)=>{
                 const errors = validation.error.details.map((detail) => detail.message);
                 return res.status(422).send(errors);
               }
-              if(!namexiste) return res.status(409).send("Usuario já cadastrado")
+              if(namexiste) return res.status(409).send("Usuario já cadastrado")
             res.status(500).send('Deu erro !!')
         }
        
@@ -112,8 +92,8 @@ app.get('/messages', async (req,res)=>{
 
         return res.send([...fullmessages].reverse())
 
-    } catch (err) {
-        console.log(err)
+    } catch (erro) {
+        console.log(erro)
 
         return res.sendStatus(500)
     }
